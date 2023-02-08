67c61d69402166bca405ca5a36e58e993e93c2ef
Alqatrony <ali.alqatrony@hotmail.com>
2023-02-08 22:23:41 +0100

    Modify to Better version

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let counter = 0;
 
-document.querySelectorAll('li').forEach(li => {
-  li.addEventListener('dblclick', function () {
-    const img = this.querySelector('.small-drop-li img');
-    const state = this.getAttribute('data-state');
+document.querySelectorAll('.small-drop-li, .text-pointer').forEach(el => {
+  el.addEventListener('click', function () {
+    const li = this.closest('li');
+    const img = li.querySelector('.small-drop-li img');
+    const state = li.getAttribute('data-state');
 
     if (state === 'hidden') {
       img.style.display = 'block';
-      this.setAttribute('data-state', 'shown');
+      li.setAttribute('data-state', 'shown');
       counter++;
     } else if (state === 'shown') {
       img.style.display = 'none';
-      this.setAttribute('data-state', 'hidden');
+      li.setAttribute('data-state', 'hidden');
       counter--;
     }
 
